332e3c1ec8b47f14c86b28654d38b8aba25b2786
NightFreddys <130055890+NightFreddys@users.noreply.github.com>
2023-05-27 19:13:37 +0200

    Create 1.bat

---
+++ 1.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Welcome to SuperUSB!
+echo Try using the "home" command and you're going to the main menu!
+:loop
+set /p commd=SuperUSB Welcome Game: 
+if %commd% == home (exit) else (goto loop)
