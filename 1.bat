b92249c2b42932a3746de08efa72040097e532eb
NightFreddys <130055890+NightFreddys@users.noreply.github.com>
2023-05-27 20:19:40 +0200

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,1 +1,1 @@
-@echo off\necho Welcome to SuperUSB!\necho Try using the "home" command and you're going to the main menu!\n:loop\nset /p commd=SuperUSB Welcome Game:\nif %commd% == home (exit) else (goto loop)
+@echo off \n echo Welcome to SuperUSB! \n echo Try using the "home" command and you're going to the main menu! \n :loop \n set /p commd=SuperUSB Welcome Game: \n if %commd% == home (exit) else (goto loop)
