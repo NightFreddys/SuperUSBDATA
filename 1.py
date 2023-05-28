d87d272a686b90087444ff70003d08422c0ef5dd
NightFreddys <130055890+NightFreddys@users.noreply.github.com>
2023-05-28 14:14:17 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,9 @@
-@echo off 
-echo Welcome to SuperUSB! 
-echo Try using the "home" command and you're going to the main menu! 
-:loop 
-set /p commd=SuperUSB Welcome Game: 
-if %commd% == home (exit) else (goto loop)
+loop = True
+well1 = 'Try using the "home" command and you'
+well2 = "'re going to the main menu!"
+print("Welcome to SuperUSB!")
+print(well1+well2)
+while loop:
+    commd = input("SuperUSB Welcome Game: ")
+    if commd=="home":
+        loop=False
